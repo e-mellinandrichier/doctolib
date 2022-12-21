3a589453fbe3b90f892b5f5411c7e3cc4ca67ea7
Mellinand Richier Emmanuelle <mellinand.emmanuelle@gmail.com>
2022-12-21 14:12:35 +0100

    doctors done

--- app/controllers/rdvs_controller.rb
+++ app/controllers/rdvs_controller.rb
@@ -1,2 +1,47 @@
 class RdvsController < ApplicationController
+  before_action :set_rdv, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @rdvs = Rdv.all
+  end
+
+  def show
+    @rdv = Rdv.find(params[:id])
+  end
+
+  def new
+    @rdv = Rdv.new # Need to instantiate the form_with
+  end
+
+  def create
+    @rdv = Rdv.new(rdv_params)
+    @rdv.save
+    redirect_to rdv_path(@rdv)
+  end
+
+  def edit
+    @rdv = Rdv.find(params[:id])
+  end
+
+  def update
+    @rdv = Rdv.find(params[:id])
+    @rdv.update(rdv_params)
+    redirect_to rdv_path(@rdv)
+  end
+
+  def destroy
+    @rdv = Rdv.find(params[:id])
+    @rdv.destroy
+    redirect_to rdvs_path, status: :see_other
+  end
+
+  private
+
+  def rdv_params
+    params.require(:rdv).permit(:date, :doctor_id, :user_id)
+  end
+
+  def set_rdv
+    @rdv = Rdv.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Doctor.create(name: "Lorde", specialty: "Melancolie", description: "vous fera bader sec")
+Doctor.create(name: "Inès", specialty: "Joie", description: "apporte soleil et bonne humeur")
+Doctor.create(name: "Jade", specialty: "Amour", description: "me manque tous les jours")
+Doctor.create(name: "Django", specialty: "Réconfort", description: "woof")
+Doctor.create(name: "Luisa", specialty: "Rires", description: "evacue toutes vos idées noires")
+Doctor.create(name: "Rilakkuma", specialty: "Confort", description: "ourson mignon")
